c10caec230fd36ed301d7188270721400e102467
sickboydroid <sickboydroid@gmail.com>
2022-11-08 16:50:12 +0530

    Add functionality to status button and add miscellaneous button

--- app/src/main/java/com/tangledbytes/j2controller/MainActivity.java
+++ app/src/main/java/com/tangledbytes/j2controller/MainActivity.java
@@ -4,13 +4,16 @@ import static com.tangledbytes.j2controller.utils.AppState.speaker;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Camera;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.tangledbytes.j2controller.music.MusicPlayerActivity;
+import com.tangledbytes.j2controller.music.SongsManager;
 import com.tangledbytes.j2controller.utils.Utils;
 
 public class MainActivity extends Activity {
@@ -25,8 +28,28 @@ public class MainActivity extends Activity {
         Utils.startAppStarterService(this);
         Button btnMusicPlayer = findViewById(R.id.btnMusicPlayer);
         Button btnToggleFlash = findViewById(R.id.btnToggleFlash);
+        Button btnStatus = findViewById(R.id.btnStatus);
+        Button btnMisc = findViewById(R.id.btnMisc);
         btnMusicPlayer.setOnClickListener((View view) -> startActivity(new Intent(this, MusicPlayerActivity.class)));
         btnToggleFlash.setOnClickListener((View view) -> toggleFlash());
+        btnStatus.setOnClickListener((View view) -> showStatus());
+        btnMisc.setOnClickListener((View view) -> misc());
+    }
+
+    private void misc() {
+    }
+
+    private void showStatus() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        int batteryPercentage = (int) ((level / (float) scale) * 100);
+        int songsAvailable = SongsManager.initializeSongManager(false).getTotalSongs();
+        speaker.say("Battery is " + batteryPercentage + " percent charge");
+        speaker.say("You have " + songsAvailable + " songs", false);
     }
 
     private void toggleFlash() {
